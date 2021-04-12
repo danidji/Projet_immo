a98c43cb2779bf95898024aa992ed3c841c676d3
dany gbelidji <dany.gbelidji@live.fr>
2021-04-12 14:54:19 +0200

    🚚 - Re-organization

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// Mise en place du moteur de template
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -18,24 +18,29 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'build'), // chemin vers le fichier sass source
+  dest: path.join(__dirname, 'public'), // chemin vers la le ficher css source
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
+
+// Mise en place du répertoire static
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//gestion des routes
+require('./routes/routes')(app)
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+module.exports = (app) => {
+
+    // route vers l'accueil
+    app.get('/', (req, res) => {
+        let Home = require('../controllers/Home');
+        (new Home()).print(req, res);
+
+    });
+
+    // route vers la page d'inscription
+    app.get('/register', (req, res) => {
+        let User = require('../controllers/Users');
+        (new User()).print(req, res);
+
+    });
+
+};

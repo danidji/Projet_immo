2e9b462efb4e26ecd9b61bfbd97634ada2965328
dany gbelidji <dany.gbelidji@live.fr>
2021-04-13 15:31:49 +0200

    ✨ MOD - Form post gestion

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 //Fichier de gestion des routes
-module.exports = (app) => {
+module.exports = (app, mongoose) => {
 
     // route vers l'accueil
     app.get('/', (req, res) => {
@@ -10,19 +10,34 @@ module.exports = (app) => {
 
     // route vers la page d'inscription
     app.get('/register', (req, res) => {
-        let User = require('../controllers/Users');
-        (new User()).print(req, res);
+        let Register = require('../controllers/Register');
+        (new Register()).print(req, res);
 
     });
 
     // gestion du formulaire post
     app.post('/register', (req, res) => {
-
         //On récupère les informations du formulaire dans req.body
-        console.log(req.body);
+        // console.log(req.body);
+        let userData = req.body
+        console.log(userData)
 
-        // On réaffiche le formulaire => possiblement une autre page plus tard
-        let User = require('../controllers/Users');
-        (new User()).print(req, res);
+
+        // Connection à la BDD
+        require('../bdd/database')(mongoose);
+
+        //création d'un nouvel utilisateur 
+        let User = require('../bdd/User');
+
+        (new User()).add(userData);
+
+
+        //fermeture de la bdd ??
+
+
+
+        // On réaffiche le formulaire 
+        let Register = require('../controllers/Register');
+        (new Register()).print(req, res);
     });
 };

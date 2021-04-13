e4ae3c44fab2b532b40d2ea1c5fe189950d1bbe1
dany gbelidji <dany.gbelidji@live.fr>
2021-04-13 17:28:13 +0200

    ADD : process form to register

--- routes/routes.js
+++ routes/routes.js
@@ -17,27 +17,31 @@ module.exports = (app, mongoose) => {
 
     // gestion du formulaire post
     app.post('/register', (req, res) => {
-        //On récupère les informations du formulaire dans req.body
-        // console.log(req.body);
-        let userData = req.body
-        console.log(userData)
 
 
-        // Connection à la BDD
-        require('../bdd/database')(mongoose);
-
-        //création d'un nouvel utilisateur 
-        let User = require('../bdd/User');
-
-        (new User()).add(userData);
-
-
-        //fermeture de la bdd ??
-
-
-
-        // On réaffiche le formulaire 
         let Register = require('../controllers/Register');
-        (new Register()).print(req, res);
+        (new Register()).process(req, res);
+
+
+        // //On récupère les informations du formulaire dans req.body
+        // // console.log(req.body);
+        // let userData = req.body
+        // console.log(userData)
+
+
+        // // Connection à la BDD
+        // require('../bdd/database')(mongoose);
+
+        // //création d'un nouvel utilisateur 
+        // let User = require('../repository/User');
+        // (new User()).add(userData);
+
+
+        // //fermeture de la bdd ??
+
+
+
+        // // On redirige l'utilisateur vers la page d'accueil
+        // res.redirect('/'); l
     });
 };

153871120ad3e1fb7ad72143b22f8aae2b9fe794
dany gbelidji <dany.gbelidji@live.fr>
2021-04-16 15:40:12 +0200

    MOD - user.js - change typos

--- src/repository/User.js
+++ src/repository/User.js
@@ -21,7 +21,7 @@ const userSchema = mongoose.Schema({
     }
     , date: { type: Date, default: Date.now }
 
-}, { versionKey: false })
+}, { versionKey: false });
 
 module.exports = class User {
     constructor() {

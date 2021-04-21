c0a5bcc142b79ae75f8b9663df9c1001427e2533
dany gbelidji <dany.gbelidji@live.fr>
2021-04-21 16:02:55 +0200

    🔨 MOD - service to upload on a specific folder

--- src/services/UploadImageRealty.js
+++ src/services/UploadImageRealty.js
@@ -1,11 +1,11 @@
 const config = require('../../app/config.js');
 
 module.exports = class UploadImageRealty {
-    moveFile(file) {
+    moveFile(file, id) {
         return new Promise((resolve, reject) => {
             const regex = /[^a-z0-9_]/i;
             let baseName = file.name.replace(regex, '_').replace('__', '_');
-            let uploadPath = config.directory_product_image + '/' + baseName;
+            let uploadPath = config.directory_product_image + '/' + id + '/' + baseName;
 
             // console.log('uploadPath :');
             console.log('uploadPath :', uploadPath);

9f040c3fc9bae7588987bfc63738db14d9599c60
dany gbelidji <dany.gbelidji@live.fr>
2021-04-23 14:14:32 +0200

    ✨ADD - image carousel on realty page

---
+++ public/javascripts/carousel_gestion.js
@@ -0,0 +1,39 @@
+const inputUrlImage = document.querySelectorAll(".url_images")
+
+// let imageCarousel = inputUrlImage.value
+
+// console.log(inputUrlImage[0].value)
+
+
+const carouselWrapper = document.querySelector(".carousel_wrapper")
+
+inputUrlImage.forEach(img => {
+    const li = document.createElement("li")
+    li.style.backgroundImage = 'url(' + img.value + ')'
+    li.className = "carousel_item"
+
+    carouselWrapper.appendChild(li)
+})
+
+let currentIndex = 0
+const propWrapperWidth = carouselWrapper.getBoundingClientRect().width
+
+const rigthButton = document.querySelector(".right")
+const leftButton = document.querySelector(".left")
+
+
+const slide = function (dir) {
+    return () => {
+        currentIndex += dir
+        if (currentIndex >= inputUrlImage.length) {
+            currentIndex = 0
+        } else if (currentIndex < 0) {
+            currentIndex = inputUrlImage.length - 1
+        }
+        const newPosition = -(propWrapperWidth / inputUrlImage.length * currentIndex)
+        carouselWrapper.style.left = newPosition + 'px'
+    }
+}
+
+rigthButton.addEventListener('click', slide(1))
+leftButton.addEventListener('click', slide(-1))

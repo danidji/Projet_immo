a87aa34b1f3e8f4cf4149f3c314f2bb869024e45
dany gbelidji <dany.gbelidji@live.fr>
2021-04-23 15:37:45 +0200

    MOD - carousel_gestion.js

--- public/javascripts/carousel_gestion.js
+++ public/javascripts/carousel_gestion.js
@@ -1,39 +1,43 @@
-const inputUrlImage = document.querySelectorAll(".url_images")
-
-// let imageCarousel = inputUrlImage.value
-
-// console.log(inputUrlImage[0].value)
+; (function (document, window) {
+    const inputUrlImage = document.querySelectorAll(".url_images")
 
 
-const carouselWrapper = document.querySelector(".carousel_wrapper")
+    const carouselWrapper = document.querySelector(".carousel_wrapper")
 
-inputUrlImage.forEach(img => {
-    const li = document.createElement("li")
-    li.style.backgroundImage = 'url(' + img.value + ')'
-    li.className = "carousel_item"
+    inputUrlImage.forEach(img => {
+        const li = document.createElement("li")
+        li.style.backgroundImage = 'url(' + img.value + ')'
+        li.className = "carousel_item"
 
-    carouselWrapper.appendChild(li)
-})
+        carouselWrapper.appendChild(li)
+    })
 
-let currentIndex = 0
-const propWrapperWidth = carouselWrapper.getBoundingClientRect().width
+    let currentIndex = 0
+    const propWrapperWidth = carouselWrapper.getBoundingClientRect().width
 
-const rigthButton = document.querySelector(".right")
-const leftButton = document.querySelector(".left")
+    const rigthButton = document.querySelector(".right")
+    const leftButton = document.querySelector(".left")
 
 
-const slide = function (dir) {
-    return () => {
-        currentIndex += dir
-        if (currentIndex >= inputUrlImage.length) {
-            currentIndex = 0
-        } else if (currentIndex < 0) {
-            currentIndex = inputUrlImage.length - 1
+    const slide = function (dir) {
+        return () => {
+            currentIndex += dir
+            if (currentIndex >= inputUrlImage.length) {
+                currentIndex = 0
+            } else if (currentIndex < 0) {
+                currentIndex = inputUrlImage.length - 1
+            }
+            const newPosition = -(propWrapperWidth / inputUrlImage.length * currentIndex)
+            carouselWrapper.style.left = newPosition + 'px'
         }
-        const newPosition = -(propWrapperWidth / inputUrlImage.length * currentIndex)
-        carouselWrapper.style.left = newPosition + 'px'
     }
-}
 
-rigthButton.addEventListener('click', slide(1))
-leftButton.addEventListener('click', slide(-1))
+    rigthButton.addEventListener('click', slide(1))
+    leftButton.addEventListener('click', slide(-1))
+
+}(document, window));
+
+
+
+
+

93e07363a73720135e60bc65bb8067637aa952f0
dany gbelidji <dany.gbelidji@live.fr>
2021-04-26 11:51:03 +0200

    fix typos

--- src/controllers/Admin.js
+++ src/controllers/Admin.js
@@ -1,7 +1,5 @@
 module.exports = class Admin {
     print(req, res) {
-        console.log('---admin JS----')
-        console.log(res.locals.session)
         res.render('admin/dashboard', {
             title: 'TeLoger'
         });

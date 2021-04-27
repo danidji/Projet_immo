7127b6b9f80da2c38edbbe4bf092d8c962f341d6
dany gbelidji <dany.gbelidji@live.fr>
2021-04-27 16:34:37 +0200

    ✨ ADD - contact repository

---
+++ src/repository/Contact.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+// Connection à la BDD
+require('../bdd/database')();
+
+//Schéma de données user
+const contactSchema = mongoose.Schema({
+    email: { type: String }
+    , nom: { type: String }
+    , date: { type: Date, default: Date.now }
+    , sujet: { type: String }
+    , message: { type: String }
+
+}, { versionKey: false });
+
+module.exports = class Contact {
+    constructor() {
+        this.db = mongoose.model('contact', contactSchema);
+    }
+
+    add(contactData) {
+        return new Promise((resolve, reject) => {
+            this.db.create(contactData, (err, doc) => {
+                console.log('élément ajouté à la bdd');
+                if (err) reject(err)
+                // console.log(doc)
+                resolve(doc)
+            })
+        })
+    }
+}

d392f577ff678cb4c1cb112c8676f8dbfb67363d
dany gbelidji <dany.gbelidji@live.fr>
2021-04-27 16:34:18 +0200

    ✨ ADD - contact controllers.js

---
+++ src/controllers/Contact.js
@@ -0,0 +1,21 @@
+let Contact = require('../repository/Contact');
+let repo = new Contact();
+
+module.exports = class Contact {
+    print(req, res) {
+        res.render('contact', { title: 'TeLoger' });
+    }
+
+    processForm(req, res) {
+        let contactData = {
+            email: req.body.email
+            , nom: req.body.nom
+            , sujet: req.body.sujet
+            , message: req.body.message
+        }
+        repo.add(contactData).then(() => {
+            req.flash('notify', 'Votre message est bien envoyé !');
+            res.redirect(`/contact`);
+        })
+    }
+}

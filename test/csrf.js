4a6b9429913619577326f47fbf4c707a7c29a447
dany gbelidji <dany.gbelidji@live.fr>
2021-04-28 14:05:16 +0200

    ✏️ MOD - test-csrf.js - add comment

--- test/csrf.js
+++ test/csrf.js
@@ -2,20 +2,18 @@
 const { doesNotMatch } = require('assert');
 const assert = require('assert');
 const csrf = require('../src/services/randomTokenCsrf');
+
+//On génère des variables représentant une (req,res)http de manière fictive, on pourra ainsi les passer en paramètre de nos fonction generate et verify
 let stubReq = {
     session: {}
     , body: {}
 };
 let stubRes = { locals: {} };
 
-function stubNext() {
-    return true
-}
-
 
 describe('test génération et validation token CSRF', () => {
     it('Génération du token', (done) => {
-
+        //                                    v--- représente le next (fct qui ne renvoie rien)
         csrf.generate(stubReq, stubRes, () => { });
         // console.log(typeof stubReq.session.token)
         assert.notStrictEqual(typeof stubReq.session.token, 'undefined');
@@ -30,7 +28,7 @@ describe('test génération et validation token CSRF', () => {
         // console.log(stubReq)
         // console.log('test', csrf.verify(stubReq, stubRes, () => true));
 
-
+        //                                                     v-- représente le next (fct qui renvoie true)
         assert.strictEqual(csrf.verify(stubReq, stubRes, () => true), true);
         done();
     })

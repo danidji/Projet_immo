30a67259f1f42f5b04fbe857624fc847824e0e35
dany gbelidji <dany.gbelidji@live.fr>
2021-04-28 16:25:44 +0200

    MOD - change config acces for heroku

---
+++ app/config.js
@@ -0,0 +1,23 @@
+if (typeof process.env.PORT == 'undefined') {
+    module.exports = require('./config-dev');
+} else {
+    module.exports = {
+        port: process.env.PORT || 3000
+        , db: {
+            id: process.env.db_id
+            , pwd: process.env.db_pwd
+            , cluster: process.env.db_cluster
+            , dbName: process.env.db_dbname
+        }
+        , appKey: process.env.appKey
+        , userDevAdmin: {
+            email: "devAdmin@yooo.univers"
+            , pass: "versLinfiniEtLauDela"
+            , civility: "male"
+            , prenom: "dev"
+            , nom: "admin"
+            , phone: "0612345678"
+        }
+        , directory_product_image: __dirname + '/../public/images/realty'
+    }
+}
